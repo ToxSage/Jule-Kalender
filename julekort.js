ed5a81623981c6f80d5a57611578676b8a457b67
ToxSage <davidwzachrisson@gmail.com>
2022-12-04 18:20:14 +0100

    Update julekort.js

---
+++ julekort.js
@@ -0,0 +1,9 @@
+var today = new Date();
+
+function dag1() {
+    if (today.getMonth() != 11) {
+        window.location.href = "https://www.youtube.com/watch?v=9bZkp7q19f0";
+    } else if (today.getDate() >= 1) {
+        window.location.href = "https://www.youtube.com/watch?v=9bZkp7q19f0";
+    }
+}
